a2c996133137a1f26997e006b9dcdaa962460929
Herr Safran <herrsafran@hm-gil.de>
2018-05-27 16:58:15 +0200

    allowed host, urls, view

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,8 +25,11 @@ SECRET_KEY = 'x+3t@b2csjcv(*5ihe_t)1w*#!zp)o30gh^1^huj^&rh(-^(k)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    'herrsafran.pythonanywhere.com',
+    '127.0.0.1',
+]
+#git aufm rechner
 
 # Application definition
 
